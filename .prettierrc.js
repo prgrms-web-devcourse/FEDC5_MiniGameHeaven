88363227aa37e98e41054b63851171e2486e82cb
SoJuSo <asdfg7123@gmail.com>
2023-10-05 14:14:07 +0900

    Add : 프리티어 파일 연결, index.js와 index.css 연결 완료

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 100, // printWidth default 80 => 100 으로 변경
+  singleQuote: true, // "" => ''
+  arrowParens: 'avoid', // arrow function parameter가 하나일 경우 괄호 생략
+  semi: true, //  세미콜론 사용 여부
+  useTabs: false, // 탭 사용 여부
+  tabWidth: 2, // // 탭 너비
+};
