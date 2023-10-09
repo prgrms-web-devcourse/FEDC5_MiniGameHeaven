907b5338af56743e7adb71e32da0e7f97223a75f
nimgnas <sangminyn@gmail.com>
2023-10-09 18:14:34 +0900

    Feat: 게임시작, 종료, 스코어

--- Games/Game3/script.js
+++ Games/Game3/script.js
@@ -2,15 +2,20 @@ import { setupGround, updateGround } from './ground.js';
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
 
 const $world = document.querySelector('[data-world]');
+const $score = document.querySelector('[data-score]');
+const $startScreen = document.querySelector('[data-start-screen]');
 
 setPixelToWorldScale();
 window.addEventListener('resize', setPixelToWorldScale);
-
-setupGround();
+document.addEventListener('keydown', handleStart, { once: true });
 
 let lastTime;
+let speedScale;
+let score;
+
 function update(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -20,13 +25,31 @@ function update(time) {
 
   const delta = time - lastTime;
 
+  updateGround(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
+
   lastTime = time;
-  updateGround(delta, 1);
   window.requestAnimationFrame(update);
 }
 
-// 1프레임마다 요청
-window.requestAnimationFrame(update);
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_SCALE_INCREASE;
+}
+
+function updateScore(delta) {
+  score += delta * 0.01;
+  $score.textContent = Math.floor(score);
+}
+
+function handleStart() {
+  lastTime = null;
+  speedScale = 1;
+  score = 0;
+  setupGround();
+  $startScreen.classList.add('hide');
+  window.requestAnimationFrame(update);
+}
 
 function setPixelToWorldScale() {
   let worldToPixelScale;
