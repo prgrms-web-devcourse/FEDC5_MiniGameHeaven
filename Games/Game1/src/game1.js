49c8ad689b8e3afe6d8699432bb617c651f44154
SoJuSo <asdfg7123@gmail.com>
2023-10-09 17:25:35 +0900

    [Feat]html에서 생성한 li태그를 JS에서 선언하도록 방식 변경

--- Games/Game1/src/game1.js
+++ Games/Game1/src/game1.js
@@ -1,1 +1,17 @@
-console.log(`js file load`);
+const $ = document;
+const imageContainer = $.querySelector('.wrap__imageContainer');
+const startButton = $.querySelector('.startButton');
+const gameCompleteText = $.querySelector('.gameCompleteText');
+const playTime = $.querySelector('.playTime');
+
+const tileCount = 16;
+
+Array(tileCount)
+  .fill()
+  .forEach((_, index) => {
+    const li = $.createElement('li');
+    li.setAttribute('data-index', index);
+    li.classList.add(`list${index}`);
+    console.log(li);
+    console.log(index);
+  });
