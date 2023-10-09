752cc6cf8f259d77e42f6b3f1efa04c9ced4ed66
Cho-Ik-Jun <harry960629@naver.com>
2023-10-09 17:35:18 +0900

    feat: 보드 위쪽 이동 구현 및 게임 오버 함수 추가

--- Games/Game4/game4.js
+++ Games/Game4/game4.js
@@ -7,6 +7,7 @@ const keyDownEventHandler = e => {
   const keyCode = e.keyCode;
   // 상
   if (keyCode === 38) {
+    moveTop();
   }
   // 하
   else if (keyCode === 40) {
@@ -17,6 +18,8 @@ const keyDownEventHandler = e => {
   // 우
   else if (keyCode === 39) {
   }
+
+  boardUpdate();
 };
 // 키보드 입력 핸들러
 document.onkeydown = keyDownEventHandler;
@@ -117,10 +120,131 @@ const changeBoxColor = box => {
   }
 };
 
+// 보드판 숫자 이동
+// 상
+const moveTop = () => {
+  let isMoved = false; // 이동 여부 확인
+  let isPlused = Array.from(Array(4), () => Array(4).fill(0)); // 중복 누적 방지
+  for (let i = 1; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (board[i][j] === 0) continue; // 숫자가 없으면 패스
+      let topY = i - 1; // 위로 이동할 좌표
+      // 이동할 칸이 빈값이 아닐 때까지 이동
+      while (topY > 0 && board[topY][j] === 0) topY--;
+      // 이동할 칸이 빈값이면
+      if (board[topY][j] === 0) {
+        // 현재값 밀어올리고 현재값 비우기
+        board[topY][j] = board[i][j];
+        board[i][j] = 0;
+        isMoved = true;
+      } // 이동할 칸이 현재값과 다를 경우
+      else if (board[topY][j] !== board[i][j]) {
+        if (topY + 1 === i) continue; // 바로 윗칸이면 패스
+        // 아니라면 바로 아래까지 밀어올리기
+        board[topY + 1][j] = board[i][j];
+        board[i][j] = 0;
+        isMoved = true;
+      } // 이동할 칸이 현재값과 같다면
+      else {
+        // 최초 이동한 칸이라면 더하기
+        if (isPlused[topY][j] === 0) {
+          board[topY][j] *= 2;
+          score += board[topY][j];
+          board[i][j] = 0;
+          isPlused[topY][j] = 1;
+          isMoved = true;
+        } // 중복 누적이라면 바로 아래칸으로 밀어올리기
+        else {
+          board[topY + 1][j] = board[i][j];
+          board[i][j] = 0;
+          isMoved = true;
+        }
+      }
+    }
+  }
+  if (isMoved) makeNewNum(); // 이동했으면 새로운 숫자 생성
+  else isGameOver(); // 아니라면 게임오버 확인
+};
+// 하
+// 좌
+// 우
+
+// 새로운 숫자 생성
+const makeNewNum = () => {
+  const zeroNum = isZero();
+  while (true) {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (board[i][j] === 0) {
+          let randNum = parseInt(Math.random() * zeroNum);
+          if (randNum === 0) {
+            board[i][j] = randNewNum();
+            return;
+          }
+        }
+      }
+    }
+  }
+};
+
+// 빈칸 갯수 세기
+const isZero = () => {
+  let isZero = 0;
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (board[i][j] === 0) isZero++;
+    }
+  }
+  return isZero;
+};
+
 // 숫자 생성 확률
 const randNewNum = () => {
   let randNum = parseInt(Math.random() * 10); // 1/10 확률
   return randNum === 0 ? 4 : 2;
+};
+
+// 최대 숫자 갱신
+const getMaxNum = () => {
+  let maxNum = 0;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      maxNum = board[i][j] > maxNum ? board[i][j] : maxNum;
+    }
+  }
+
+  return maxNum;
+};
+
+// 게임오버 확인
+const isGameOver = () => {
+  // 행 체크
+  for (let i = 0; i < 4; i++) {
+    let tempCol = board[i][0];
+    if (tempCol === 0) return; // 빈 칸이 존재하면 게임 가능
+    for (let j = 1; j < 4; j++) {
+      // 빈 칸이거나 왼쪽 칸과 동일하면 게임 가능
+      if (board[i][j] === tempCol || board[i][j] === 0) return;
+      // 값이 있으면 갱신
+      else tempCol = board[i][j];
+    }
+  }
+  // 열 체크
+  for (let i = 0; i < 4; i++) {
+    let tempRow = board[0][i];
+    if (tempRow === 0) return; // 빈 칸이 존재하면 게임 가능
+    for (let j = 1; j < 4; j++) {
+      // 빈 칸이거나 왼쪽 칸과 동일하면 게임 가능
+      if (board[j][i] === tempRow || board[j][i] === 0) return;
+      // 값이 있으면 갱신
+      else tempRow = board[j][i];
+    }
+  }
+
+  // 게임 불가능하면 최고 점수 반환 및 alert
+  alert(`게임 오버입니다. 최대 숫자 : ${getMaxNum()} , 최고 점수 : ${score}`);
+  init();
 };
 
 init();
