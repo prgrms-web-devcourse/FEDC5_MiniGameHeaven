1fa205c68a50b007acf2a4fda7a66e98bbe039ae
SoJuSo <asdfg7123@gmail.com>
2023-10-09 17:33:42 +0900

    [Feat] 이미지를 생성하는 createImageTiles()와 이미지를 랜덤하게 섞어주는 imageShuffle() 함수 생성 완료, 이미지 섞기 완료

--- Games/Game1/src/game1.js
+++ Games/Game1/src/game1.js
@@ -6,12 +6,34 @@ const playTime = $.querySelector('.playTime');
 
 const tileCount = 16;
 
-Array(tileCount)
-  .fill()
-  .forEach((_, index) => {
-    const li = $.createElement('li');
-    li.setAttribute('data-index', index);
-    li.classList.add(`list${index}`);
-    console.log(li);
-    console.log(index);
-  });
+let tiles = [];
+tiles = createImageTiles();
+console.log(tiles);
+
+imageShuffle(tiles).forEach(tile => {
+  imageContainer.appendChild(tile);
+});
+
+function createImageTiles() {
+  const tempArray = [];
+  Array(tileCount)
+    .fill()
+    .forEach((_, index) => {
+      const li = $.createElement('li');
+      li.setAttribute('data-index', index);
+      li.classList.add(`list${index}`);
+      tempArray.push(li);
+    });
+
+  return tempArray;
+}
+
+function imageShuffle(array) {
+  let index = array.length - 1;
+  while (index > 0) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+    index--;
+  }
+  return array;
+}
