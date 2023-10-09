905b6a08ce5783bb4ebfb8783c3413e81bc9f8e6
Cho-Ik-Jun <harry960629@naver.com>
2023-10-09 15:06:09 +0900

    feat: 키보드 입력 핸들러 추가

--- Games/Game4/game4.js
+++ Games/Game4/game4.js
@@ -1,3 +1,22 @@
 let board = Array.from(Array(4), () => Array(4).fill(0));
 let boardId = Array.from([0, 1, 2, 3], n => Array(`${n}0`, `${n}1`, `${n}2`, `${n}3`));
 let score;
+
+// 키보드 입력
+const keyDownEventHandler = e => {
+  const keyCode = e.keyCode;
+  // 상
+  if (keyCode === 38) {
+  }
+  // 하
+  else if (keyCode === 40) {
+  }
+  // 좌
+  else if (keyCode === 37) {
+  }
+  // 우
+  else if (keyCode === 39) {
+  }
+};
+// 키보드 입력 핸들러
+document.onkeydown = keyDownEventHandler;
