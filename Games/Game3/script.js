4c0db5254e5e02879811c987d7ead8043198489e
nimgnas <sangminyn@gmail.com>
2023-10-09 15:55:49 +0900

    Feat: 기본 프레임 요청 함수, 플레이 화면 비율 설정 함수"

---
+++ Games/Game3/script.js
@@ -0,0 +1,37 @@
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+
+const $world = document.querySelector('[data-world]');
+
+setPixelToWorldScale();
+window.addEventListener('resize', setPixelToWorldScale);
+
+let lastTime;
+function update(time) {
+  if (lastTime === null) {
+    lastTime = time;
+    window.requestAnimationFrame(update);
+    return;
+  }
+
+  const delta = time - lastTime;
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+
+// 1프레임마다 요청
+window.requestAnimationFrame(update);
+
+function setPixelToWorldScale() {
+  let worldToPixelScale;
+
+  // window의 크기비율에 따라 world의 사이즈 조정
+  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+  } else {
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
+  }
+
+  $world.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+  $world.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
+}
