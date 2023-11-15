565aaa072d5c35e96e1f17010e3aaf6de7ee1bda
yejinleee <yejin9487@gmail.com>
2023-11-15 14:27:21 +0900

    feat: 유저이름 입력, 로컬저장 및 모달 효과

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hello');
+const $input = document.querySelector('.modal');
+const $container = document.querySelector('.container');
+const $inputBtn = document.querySelector('.input-button');
+
+let value = '';
+function isUsername() {
+  const username = localStorage.getItem('username');
+  return username ? true : false;
+}
+$input.style.opacity = isUsername() ? 0 : 1;
+$container.style.opacity = isUsername() ? 1 : 0;
+
+function checkUsername(value) {
+  if (value.trim() !== '') {
+    localStorage.setItem('username', value);
+    $container.style.opacity = isUsername() ? 1 : 0;
+    document.querySelector('.username-input').value = '';
+    $input.style.opacity = 0;
+  }
+}
+$input.addEventListener('keyup', e => {
+  e.preventDefault();
+  value = e.target.value;
+  if (e.key === 'Enter') {
+    checkUsername(value);
+  }
+});
+$inputBtn.addEventListener('click', e => {
+  checkUsername(value);
+});
